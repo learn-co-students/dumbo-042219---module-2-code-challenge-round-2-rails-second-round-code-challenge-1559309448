f247b3d4ba0c352ff4d89a61d093ad69d2968843
Chloe <gingkolane@gmail.com>
2019-05-31 13:21:41 -0400

    done

--- app/controllers/vendor_sweets_controller.rb
+++ app/controllers/vendor_sweets_controller.rb
@@ -9,17 +9,23 @@ class VendorSweetsController < ApplicationController
   end 
 
   def create
-    @vendor_sweet = VendorSweet.create(vendor_sweet_params)
-    redirect_to vendors_path
+    @vendor_sweet = VendorSweet.new(vendor_sweet_params)
+
+    if @vendor_sweet.price.valid? 
+      @vendor_sweet.save
+      redirect_to vendor_path(vendor_sweet_params[:vendor_id])
+    else
+      flash[:errors] = @vendor_sweet.errors.full_messages
+      redirect_to new_vendor_sweet_path
+    end
   end 
 
-
-  def edit
+  def edit 
     @vendor_sweet = VendorSweet.find(params[:id])
-    @vendor_sweet.update(vendor_sweet_params)
-  end 
+  end
 
   private
+
   def vendor_sweet_params
     params.require(:vendor_sweet).permit(:price, :sweet_id, :vendor_id)
   end 
